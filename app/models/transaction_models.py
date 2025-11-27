08bf7e872adaae6a6595a82bb640253516532ce4
Sameer Mujawar <sameer.mujawar@taodigitalsolutions.com>
2025-11-27 13:49:09 +0530

    request optional field removed

--- app/models/transaction_models.py
+++ app/models/transaction_models.py
@@ -7,18 +7,11 @@ from typing import Optional
 class TransactionRequest(BaseModel):
     transaction_id: Optional[str] = None
     timestamp: datetime
-
     user_id: Optional[str] = None
     card_number: Optional[str] = None
-
     amount: float
     currency: str
-    transaction_type: Optional[str] = None
-    payment_channel: Optional[str] = None
-
-    device_id: Optional[str] = None
     ip_address: Optional[str] = None
-
     current_latitude: Optional[float] = None
     current_longitude: Optional[float] = None
     current_country: Optional[str] = None
